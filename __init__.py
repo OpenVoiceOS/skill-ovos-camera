e1ab7cd53b1f33f69d2f534759c6f5bca61dddbf
miro <jarbasai@mailfence.com>
2025-01-06 19:23:28 +0000

    session support

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ import random
 import time
 from os.path import dirname, exists, join
 from ovos_bus_client.message import Message
+from ovos_bus_client.session import SessionManager, Session
 from ovos_utils import classproperty
 from ovos_utils.process_utils import RuntimeRequirements
 from ovos_workshop.decorators import intent_handler
@@ -24,14 +25,15 @@ class WebcamSkill(OVOSSkill):
                                    no_gui_fallback=True)
 
     def initialize(self):
-        self.has_camera = False
+        self.sess2cam = {}
         if "play_sound" not in self.settings:
             self.settings["play_sound"] = True
         self.add_event("ovos.phal.camera.pong", self.handle_pong)
         self.bus.emit(Message("ovos.phal.camera.ping"))
 
     def handle_pong(self, message: Message):
-        self.has_camera = True
+        sess = SessionManager.get(message)
+        self.sess2cam[sess.session_id] = True
 
     @property
     def pictures_folder(self) -> str:
@@ -46,9 +48,18 @@ class WebcamSkill(OVOSSkill):
             if exists(s):
                 self.play_audio(s, instant=True)
 
+    @intent_handler("have_camera.intent")
+    def handle_camera_check(self, message):
+        sess = SessionManager.get(message)
+        if not self.sess2cam.get(sess.session_id):
+            self.speak_dialog("camera_error")
+        else:
+            self.speak_dialog("camera_yes")
+
     @intent_handler("take_picture.intent")
     def handle_take_picture(self, message):
-        if not self.has_camera:
+        sess = SessionManager.get(message)
+        if not self.sess2cam.get(sess.session_id):
             self.speak_dialog("camera_error")
             return
 
